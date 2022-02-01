361a3b8d4d9b2b86cf93d37d73a234aa577ce614
juancamilosalazarrestrepo <juancamilosalazarrestrepo@gmail.com>
2022-02-01 14:49:57 -0500

    crud angular completo editar add delete

--- CrudBasico/src/app/COMPONENTES/modificar/modificar.component.ts
+++ CrudBasico/src/app/COMPONENTES/modificar/modificar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Equipo, EquipoService} from '../../SERVICES/equipo.service';
-import {Router} from '@angular/router';
+import {Router, ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-modificar',
@@ -13,13 +13,41 @@ export class ModificarComponent implements OnInit {
     id_equipo: '',
     nombre:'',
     logo:''
-    };
-  constructor(private EquipoService:EquipoService,private router:Router) { }
+    } ;
+  constructor(private EquipoService:EquipoService,
+              private router:Router,
+              private activedRoute:ActivatedRoute
+              ) { }
 
   ngOnInit(): void {
+    const id_entrada = <string>this.activedRoute.snapshot.params?.['id'];
+    console.log(id_entrada)
+   
+
+    if(id_entrada){
+      this.EquipoService.getUnEquipo(id_entrada).subscribe(
+        res=>{
+          this.equipo =<any>res[0];
+          console.log(this.equipo);
+        },
+        err=>console.log(err)
+
+      );
+
+
+    }
+
   }
 
   modificar(){
+    this.EquipoService.editEquipo(this.equipo.id_equipo || ' ', this.equipo).subscribe(
+      res=>{
+        console.log(res);
+      },
+      err=>console.log(err)
+      
+    );
+    this.router.navigate(['/inicio']);
 
   }
 
